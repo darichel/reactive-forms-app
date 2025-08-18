439457063f70ed56b5a43bed74c92cb600f26ad0
Darichel <darichel.perez@gmail.com>
2025-08-18 19:04:50 -0300

    Se agrego margin y el side menu

--- src/app/shared/components/side-menu/side-menu.component.ts
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -1,8 +1,24 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { reactiveRoutes } from '../../../reactive/pages/reactive.routes';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuItem {
+  route: string;
+  title: string;
+}
+
+const reactiveItems = reactiveRoutes[0].children ?? [];
 
 @Component({
   selector: 'app-side-menu',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu.component.html',
 })
-export class SideMenuComponent { }
+export class SideMenuComponent {
+  activeMenu: MenuItem[] = reactiveItems
+  .filter(item => item.title !== undefined)
+  .map((item) => ({
+    route: `reactive/${item.path}`,
+    title: `${item.title}`,
+  }));
+}
