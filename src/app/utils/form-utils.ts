bf6f8836537325c2e51db33be5f56d7f44d878ff
Darichel Perez <darichel.perez@gmail.com>
2025-08-20 15:10:16 -0300

    Mostrando errores en pantalla

--- src/app/utils/form-utils.ts
+++ src/app/utils/form-utils.ts
@@ -1,4 +1,4 @@
-import { FormGroup } from "@angular/forms";
+import { FormArray, FormGroup, ValidationErrors } from '@angular/forms';
 
 export class FormUtils {
 
@@ -7,11 +7,42 @@ export class FormUtils {
   }
 
   static getFieldError(form: FormGroup, field: string): string | null {
-    const errors = form.controls[field].errors;
-    if (errors?.['required']) return 'Este campo es requerido.';
-    if (errors?.['minlength']) return `El mínimo es de ${errors['minlength'].requiredLength} caracteres.`;
-    if (errors?.['min']) return `El valor mínimo es ${errors['min'].min}.`;
-    return null;
+    if (!form.controls[field]) return null;
+    const errors = form.controls[field].errors ?? {};
+    return this.getErrorMessage(errors);
   }
 
+  static isValidFieldInArray(
+    formArray: FormArray,
+    index: number
+  ): boolean | null {
+    return (
+      formArray.controls[index].errors && formArray.controls[index].touched
+    );
+  }
+
+  static getFieldErrorInArray(
+    formArray: FormArray,
+    index: number
+  ): string | null {
+    if (formArray.controls.length == 0) return null;
+    const errors = formArray.controls[index].errors ?? {};
+    return this.getErrorMessage(errors);
+  }
+
+  private static getErrorMessage(errors: ValidationErrors): string | null{
+    for (let key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido.';
+        case 'minlength':
+          return `El mínimo es de ${errors['minlength'].requiredLength} caracteres.`;
+        case 'min':
+          return `El valor mínimo es ${errors['min'].min}.`;
+        default:
+          return 'Error desconocido.';
+      }
+    }
+    return null;
+  }
 }
