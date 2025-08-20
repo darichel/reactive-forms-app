92f69367c26567121655c22c273c91696db91005
Darichel Perez <darichel.perez@gmail.com>
2025-08-20 16:21:36 -0300

    Switches checks y redio buttons

--- src/app/reactive/pages/switches-pages/switches-pages.component.ts
+++ src/app/reactive/pages/switches-pages/switches-pages.component.ts
@@ -1,9 +1,31 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  Form,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-switches-pages',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './switches-pages.component.html',
 })
-export class SwitchesPagesComponent { }
+export class SwitchesPagesComponent {
+  private fb = inject(FormBuilder);
+  formUtils = FormUtils;
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true],
+    termsAndConditions: [false, Validators.requiredTrue],
+  });
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+    console.log(this.myForm.value);
+  }
+
+}
